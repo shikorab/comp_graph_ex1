03f4f30d3033f2d9d4559e4746d20c486fbaaba2
Roei Herzig <roeiherz@gmail.com>
2016-04-09 10:12:06 +0300

    add angle

---
+++ RayTracer/src/RayTracerObj/Angle.java
@@ -0,0 +1,14 @@
+package RayTracerObj;
+
+public class Angle {
+	final private double rx;
+	final private double ry;
+	final private double rz;
+
+	public Angle(double rx, double ry, double rz) {
+		this.rx = rx;
+		this.ry = ry;
+		this.rz = rz;
+	}
+
+}
